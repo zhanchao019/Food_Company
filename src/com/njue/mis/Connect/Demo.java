6e6d3310496ef8b8d9097299d793705ceb169c8c
zhanchao019 <1343145329@qq.com>
2019-12-14 19:33:54 +0800

    添加了简单的数据接口，可以用来快速查询数据

---
+++ src/com/njue/mis/Connect/Demo.java
@@ -0,0 +1,31 @@
+package com.njue.mis.Connect;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Demo {
+
+    static String sql = null;
+    static ConnectionBuilder db1 = null;
+    static ResultSet ret = null;
+
+    public static void main(String[] args) {
+        sql = "select id from tb_goods";//SQL语句
+        db1 = new ConnectionBuilder(sql);//创建DBHelper对象
+
+        try {
+            ret = db1.pst.executeQuery();//执行语句，得到结果集
+            while (ret.next()) {
+                String uid = ret.getString(1);
+
+                System.out.println(uid);
+            }//显示数据
+            ret.close();
+            db1.close();//关闭连接
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/com/njue/mis/view/images/CounterDeptFrame.java
@@ -0,0 +1,31 @@
+/*
+ * Created by JFormDesigner on Sat Dec 14 19:21:46 CST 2019
+ */
+
+package com.njue.mis.view.images;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Brainrain
+ */
+public class CounterDeptFrame extends JFrame {
+    public CounterDeptFrame() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
